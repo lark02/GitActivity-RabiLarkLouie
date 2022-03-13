88e681564063c69870e63d4a296079421c232dca
RabiLarkLouie <louierabi@gmail.com>
2022-03-13 18:06:26 +0800

    My Educational Background added

--- git-python-rabi.py
+++ git-python-rabi.py
@@ -5,3 +5,9 @@ Created on Sun Mar 13 17:46:48 2022
 @author: Rabi Family
 """
 
+print ("Lark Louie M. Rabi")
+print ("Lupac, Boac, Marinduque")
+print ("May 2, 2002")
+ 
+print ("Marinduque State College")
+print ("BSI\T-2B")
